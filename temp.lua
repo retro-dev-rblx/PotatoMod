05378d2f20fda830a0c593251dc2ab4a987b1803
NicePotato-MS <101688727+NicePotato-MS@users.noreply.github.com>
2024-02-10 20:05:28 -0600

    Delete temp.lua

--- temp.lua
+++
@@ -1,8 +0,0 @@
-local remoteEvent = game.Workspace.RemoteEvent
-
-local function fireRemoteEvent(param1, param2, param3, param4, param5)
-    remoteEvent:FireServer(param1, param2, param3, param4, param5)
-end
-
-print("firing")
-fireRemoteEvent("This is just a string","","","","")
