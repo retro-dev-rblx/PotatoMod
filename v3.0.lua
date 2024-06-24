54ff16b770137256b5714d9aeaa07feb006812d9
= <micahsuessgithub@gmail.com>
2024-06-24 16:34:47 -0500

    whoops

---
+++ v3.0.lua
@@ -0,0 +1,19 @@
+local PM = {
+    hooks = {},
+    meta = {}
+}
+
+local hooks = PM.hooks
+local meta = PM.meta
+
+function meta.
+
+function PM.HookInstance(instance)
+
+end
+
+local function registerStudio()
+
+end
+
+registerStudio()
